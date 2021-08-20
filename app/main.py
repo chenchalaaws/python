db79b060127dee70928676e791fcbb93e151fded
unknown <chenchala.srinu@gmai.com>
2021-08-20 16:54:14 +0530

    latest

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
-from flask import Flask,request
-from flask_cors import CORS
-
-app=Flask(__name__)
-CORS(app)
-
-
-@app.route('/get-name',methods=['get'])
-def fnGetName():
+from flask import Flask
+  
+app = Flask(__name__)
+  
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
+    
+@app.route("/get-name",methods=["get"])
+def getName():
     return "srinu"
-
-
