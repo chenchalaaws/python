fb625a1409e993abd6fb66021700364dcdb2fdfa
unknown <chenchala.srinu@gmai.com>
2021-08-20 17:08:32 +0530

    latest

--- app/main.py
+++ app/main.py
@@ -2,10 +2,6 @@ from flask import Flask
   
 app = Flask(__name__)
   
-@app.route("/")
-def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
-    
 @app.route("/get-name",methods=["get"])
 def getName():
     return "srinu"
